74d7495a795a16b5302b80b108f794bb59d395f3
Jean-baptiste JOFFRE <jean-baptiste.joffre@externe.maif.fr>
2021-06-11 10:53:43 +0200

    init project

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Application = () => <div>Mon application</div>;
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Application />);
